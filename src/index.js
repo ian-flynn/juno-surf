44197ad5ceccce19431d0b3549bd19094a9bdc29
Ian Flynn <ianflynnwork@gmail.com>
2023-06-14 20:18:40 -0400

    remove redundant imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
 import App from './components/App.jsx';
-import styles from './styles/styles.scss';
+import './styles.scss';
 import * as ReactDOMClient from 'react-dom/client';
 
 const container = document.getElementById('root');
