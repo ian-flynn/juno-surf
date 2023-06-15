7f71d94b875a5d6a57e0da5732f145bfb85436ee
Ian Flynn <ianflynnwork@gmail.com>
2023-06-14 20:11:23 -0400

    remove React 16 render

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './components/App.jsx';
 import styles from './styles/styles.scss';
 import * as ReactDOMClient from 'react-dom/client';
 
-// render(<App />, document.getElementById('root'));
 const container = document.getElementById('root');
 const root = ReactDOMClient.createRoot(container);
 root.render(<App />);
