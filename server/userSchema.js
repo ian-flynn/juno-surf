af72a55ed556ae4377adf38ece3a375747ad0ee5
Ian Flynn <ianflynnwork@gmail.com>
2023-06-20 15:22:40 -0400

    basic usershema setup

--- server/userSchema.js
+++ server/userSchema.js
@@ -1,1 +1,9 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  beachCode: { type: String, default: 'temp' },
+});
+
+module.exports = mongoose.model('User', userSchema);
